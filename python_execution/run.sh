cdc618c1863a7fd507ab30aef3d375aeb0ecd1c7
Dominik Probst <Dominik.Probst@htw-berlin.de>
2023-07-03 15:15:04 +0200

    add dockerfile

---
+++ python_execution/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <python_script> <directory> [additional_parameters...]"
+    exit 1
+fi
+
+python_script=$1
+directory=$2
+
+shift 2
+
+. /home/cc/.local/red/bin/activate
+export PYTHONPATH=$PYTHONPATH:$directory/NT1-HRV
+
+python3 "$directory/$python_script" "$@" 2>&1
+echo ""
